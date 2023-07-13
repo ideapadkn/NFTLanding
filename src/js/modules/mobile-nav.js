e6e3cb095a7216ebd754693e450c2b2010be8522
Kamron or Камрон <kamrush6429@gmail.com>
2023-07-13 15:37:47 +0500

    finish header block

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -4,11 +4,11 @@ function mobileNav() {
 	const nav = document.querySelector('.mobile-nav');
 	const menuIcon = document.querySelector('.nav-icon');
 
-	navBtn.onclick = function () {
+	navBtn.addEventListener('click', () => {
 		nav.classList.toggle('mobile-nav--open');
 		menuIcon.classList.toggle('nav-icon--active');
 		document.body.classList.toggle('no-scroll');
-	};
+	})
 }
 
 export default mobileNav;
