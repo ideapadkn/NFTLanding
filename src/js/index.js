944e8e5938f0df8c885dbd14182348d526d05e2f
Kamron or Камрон <kamrush6429@gmail.com>
2023-07-21 23:03:59 +0500

    added form validation and fixed some bugs to responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,35 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+
+
+// FORM VALIDATION
+
+function validation(form) {
+  let result = true;
+  const allInputs = document.querySelectorAll("input");
+
+  for (const input of allInputs) {
+    if (input.value === "") {
+      input.classList.add("_error");
+      result = false;
+    } else if (input.classList.contains("_error")) {
+      input.classList.remove("_error");
+    } else {
+      input.value = "";
+    }
+  }
+
+  return result;
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (validation(this) === true) {
+    alert("Successful");
+  } else {
+    alert("Error");
+  }
+});
